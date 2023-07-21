6a3e8dc445764051fb73b4031ce92c09ed22a39d
Mohammed A <111096981+Xor01@users.noreply.github.com>
2023-07-21 23:38:21 +0300

    create the editing function
    when click on update on the card user is redirected to edit.html with the corresponding movie id to update either rating or review of both

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
 import requests
 from dotenv import load_dotenv
 from os import getenv
 from movie_tabel import db, Movie
+from rate_movie_form import RateMovieForm
 
 load_dotenv()
 app = Flask(__name__)
@@ -23,6 +21,22 @@ with app.app_context():
 def home():
     movies = Movie.query.all()
     return render_template("index.html", movies=movies)
+
+
+@app.route('/edit/<int:movie_id>', methods=['POST', 'GET'])
+def edit(movie_id):
+    movie = db.get_or_404(Movie, movie_id)
+    form = RateMovieForm()
+    if request.method == 'POST':
+        new_rating = request.form.get('rating')
+        new_review = request.form.get('review')
+        with app.app_context():
+            movie_to_update = db.get_or_404(Movie, movie_id)
+            movie_to_update.rating = new_rating if new_rating else movie_to_update.rating
+            movie_to_update.review = new_review if new_review else movie_to_update.review
+            db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('edit.html', movie=movie, form=form)
 
 
 if __name__ == '__main__':
---
+++ rate_movie_form.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, FloatField
+
+
+class RateMovieForm(FlaskForm):
+    rating = FloatField(label='Your Rating Out of 10 e.g. 9.5')
+    review = StringField(label='Your Review')
+    update = SubmitField(label='Update')
