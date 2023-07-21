e87286d14488ccb0232070cb67e386073e99e6e5
Mohammed A <111096981+Xor01@users.noreply.github.com>
2023-07-21 21:44:03 +0300

    adding movies_table importing it in app.py

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
-from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 from dotenv import load_dotenv
 from os import getenv
-
+from movie_tabel import db, Movie
 
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('MOVIES_URI')
 Bootstrap5(app)
+db.init_app(app=app)
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
 def home():
+    mv = db.get_or_404(Movie, 1)
+    print(mv.title)
     return render_template("index.html")
 
 
---
+++ movie_tabel.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(500))
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer)
+    review = db.Column(db.String(500))
+    img_url = db.Column(db.String(500))
