fbbae4223e29d076c96e814cfa8f6bb44326c419
Mohammed A <111096981+Xor01@users.noreply.github.com>
2023-07-21 23:51:35 +0300

    change name to update for more specific

--- update_movie_form.py
+++ update_movie_form.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, FloatField
 
 
-class RateMovieForm(FlaskForm):
+class UpdateMovieForm(FlaskForm):
     rating = FloatField(label='Your Rating Out of 10 e.g. 9.5')
     review = StringField(label='Your Review')
     update = SubmitField(label='Update')
